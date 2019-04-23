bf1ecce26057e844bd1f4068eba2a75fb39f0866
Adam Bibby <Adam_Bibby@hotmail.com>
2019-04-23 09:09:43 -0400

    dont randomly open the wheather

--- i3gobar/main.go
+++ i3gobar/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os/exec"
 	"time"
 
 	"github.com/zwzn/i3/i3gobar/bar"
@@ -11,13 +12,17 @@ func main() {
 
 	bar.Run(
 		bar.Schedule(modules.Weather, time.Hour).OnClick(func(click bar.Click) {
+			if click.Button != 1 {
+				return
+			}
 			modules.Shell("brave 'https://weather.gc.ca/city/pages/on-5_metric_e.html'")()
 		}),
 		modules.Music(),
 		bar.Schedule(modules.ZFS, time.Hour),
 		bar.Schedule(modules.Mail, time.Minute),
 		bar.Schedule(modules.Pamac, time.Hour).OnClick(func(click bar.Click) {
-			modules.Notify(modules.Shell("pamac checkupdates")()).AppName("pamac").Send()
+			b, _ := exec.Command("pamac", "checkupdates").Output()
+			modules.Notify(string(b)).AppName("pamac").Send()
 		}),
 		bar.Schedule(modules.Shell("$HOME/bin/battery"), time.Second*10),
 		bar.Schedule(modules.Prepend("MST ", modules.TimeIn("America/Phoenix")), time.Second),
